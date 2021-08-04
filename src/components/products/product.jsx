159bfe00482f5aa6fc27edc2f44f30d919ab5100
sarisgar28 <sarisgar28@gmail.com>
2021-08-04 16:37:54 -0400

    product

--- src/components/products/product.jsx
+++ src/components/products/product.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import{Card, CardMedia,CardContent,CardActions,Typography, IconBotton} from '@material-ui/core'
-import { AddShoppingCart, CropLandscapeSharp } from '@material-ui/icons';
+import { AddShoppingCart} from '@material-ui/icons';
 import { classes } from 'istanbul-lib-coverage';
 
 const product = ({product}) => {
