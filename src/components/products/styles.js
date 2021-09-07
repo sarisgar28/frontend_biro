a3dc7385aa50667bdb156f2818152e15a3875113
sarisgar28 <sarisgar28@gmail.com>
2021-09-07 08:27:41 -0400

    progress

--- src/components/products/styles.js
+++ src/components/products/styles.js
@@ -1,20 +1,13 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
   root: {
-    // maxWidth: 345, original width style
-    maxWidth: '100%',
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  cardActions: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-  },
-  cardContent: {
-    display: 'flex',
-    justifyContent: 'space-between',
+    flexGrow: 1,
   },
 }));
